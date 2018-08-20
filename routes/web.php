1f7b96f4286ef6349172abb5a197061a8076173b
Ronaldo <ronaldofarfan14715@gmail.com>
2018-08-19 22:29:22 -0500

    commit 4

--- routes/web.php
+++ routes/web.php
@@ -6,28 +6,24 @@ Route::get('/', function(){
 
 });
 
-Route::get('/usuarios',function(){
-    
-    return 'Usuario';
-});
+Route::get('/usuarios','UserController@index');
 
 
-Route::get('/usuarios/{id}', function($id){
+Route::get('/usuarios/{id}','UserController@show')
 
-    return "Mostrando el detalle del usuario {$id}";
-})->where('id','[0-9]+');
+    ->where('id','[0-9]+');
 
-Route::get('/usuarios/nuevo', function(){
-    return "Crear nuevo usuario";
-});
+Route::get('/usuarios/nuevo', 'UserController@create');
 
-Route::get('/usuarios/{name}/{nickname?}', function($name,$nickname= null){
+Route::get('/saludo/{name}/{nickname?}', function($name,$nickname= null){
 
 
+    $name = ucfirst($name);
 
-    if(!$nickname){
-        return "Bievenido {$name} no tienes apodo";
-    }else{
+    if($nickname){
         return "Bienvenido {$name} tu apodo es {$nickname}";
+    }else{
+        
+        return "Bienvenido {$name}";
     }
 });
--- tests/Feature/UserModuleTest.php
+++ tests/Feature/UserModuleTest.php
@@ -8,15 +8,48 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserModuleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    /** @test */
+    public function prueba_carga_usuarios()
     {
         $this->get('/usuarios')
             ->assertStatus(200)
             ->assertSee('Usuario');
     }
+
+
+    /** @test */
+
+    function prueba_carga_detalles_usuarios(){
+        $this->get('/usuarios/5')
+            ->assertStatus(200)
+            ->assertSee('Mostrando el detalle del usuario 5');
+
+    }
+    /** @test */
+
+    function prueba_carga_crear_usuario(){
+        $this->get('/usuarios/nuevo')
+            ->assertStatus(200)
+            ->assertSee('Crear nuevo usuario');
+    }
+
+    /** @test */
+
+    function prueba_carga_saludo_nickname(){
+        $this->get('/saludo/ronaldo/bully')
+            ->assertStatus(200)
+            ->assertSee('Bienvenido Ronaldo tu apodo es bully');
+    }
+
+
+    /** @test */
+
+    function prueba_carga_saludo_no_nickname(){
+        $this->get('/saludo/ronaldo')
+            ->assertStatus(200)
+            ->assertSee('Bienvenido Ronaldo');
+    }
+
+
+    
 }
